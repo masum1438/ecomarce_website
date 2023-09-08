b584c3232b196b979f10de2981af23cea0a27941
masum1438 <masumahmed1438@gmail.com>
2023-09-08 22:30:30 +0600

    first commit

---
+++ js/price.js
@@ -0,0 +1,459 @@
+var div=document.getElementById('card7');
+var divv=document.getElementById('card8');
+var divvv=document.getElementById('section');
+var bk=document.getElementById('bkash');
+var ng=document.getElementById('nagad');
+var cp=document.getElementById('coupon');
+var hs=document.getElementById('history');
+var pmnt=document.getElementById('payment');
+var v6=0,v1=0,v2=0,v3=0,v4=0,v5=0,mm=0;
+function hideb(){
+    bk.classList.remove('hidden');
+    pmnt.classList.remove('hidden');
+    ng.classList.add('hidden');
+    
+   }function hiden(){
+    pmnt.classList.remove('hidden');
+    bk.classList.add('hidden');
+    ng.classList.remove('hidden');
+    
+   }
+
+   function hide(){
+    divvv.classList.remove('hidden');
+    div.classList.add('hidden');
+    divv.classList.add('hidden');
+    hs.classList.add('hidden');
+    cp.classList.add('hidden');
+   
+    
+     
+   }
+   function hide2(){
+    hs.classList.add('hidden');
+    cp.classList.add('hidden');
+    div.classList.remove('hidden');
+    divvv.classList.add('hidden');
+    divv.classList.add('hidden');
+    pmnt.classList.add('hidden'); 
+    bk.classList.add('hidden');
+    ng.classList.add('hidden');
+    var m=0;
+    setelement('total2',m);
+    setelement('total3',m);
+    v1=0;v2=0;v3=0;v4=0;v5=0;v6=0; 
+    setelement('totalprice2',mm);
+    setelement('total',mm);
+    setelement('discount',mm);
+    setelement('totalprice',mm);
+    setelement('it6',v6);
+    setelement('it5',v5);
+    setelement('it4',v4);
+    setelement('it3',v3);
+    setelement('it2',v2);
+    setelement('it1',v1);
+   }
+   function hide3(){
+    hs.classList.add('hidden');
+    cp.classList.add('hidden');
+    divvv.classList.remove('hidden');
+    div.classList.add('hidden');
+    divv.classList.add('hidden');
+    pmnt.classList.add('hidden');
+    bk.classList.add('hidden');
+    ng.classList.add('hidden');
+    var m=0;
+    setelement('total2',m); 
+    setelement('total3',m); 
+    v1=0;v2=0;v3=0;v4=0;v5=0;v6=0; 
+    setelement('totalprice2',mm);
+    setelement('total',mm);
+    setelement('discount',mm);
+    setelement('totalprice',mm);
+    setelement('it6',v6);
+    setelement('it5',v5);
+    setelement('it4',v4);
+    setelement('it3',v3);
+    setelement('it2',v2);
+    setelement('it1',v1);
+   }
+   function show(){
+    divv.classList.remove('hidden');
+    cp.classList.remove('hidden');
+    hs.classList.remove('hidden');
+    divvv.classList.add('hidden');
+    div.classList.add('hidden');
+     
+   }
+   function show2(){  
+    hs.classList.add('hidden');   
+     
+   }
+document.getElementById('btn-coupon').addEventListener('click',function(){
+    show2();
+    var Discount=0.0;
+    const prevtotal=getelementbytext('totalprice2');
+    const total=parseFloat(prevtotal);
+    const couponvalue=getelementbyinput('coupon-input');
+    const coupon="SELL200";
+    
+    if(total>=2000){
+        if(couponvalue==coupon){
+        Discount=total*0.2;}
+    }
+  
+    const newtotal=total-Discount;
+    setelement('discount',Discount);
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal);
+
+   })
+document.getElementById('card1').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('accessories');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;    
+    const newtotal=total;
+   v1++;
+    setelement('it1',v1);
+    setelement('totalprice',total);   
+    setelement('totalprice2',total);   
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+document.getElementById('car1').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('accessories');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;    
+    const newtotal=total;
+   v1++;
+    setelement('it1',v1);
+    setelement('totalprice',total);
+    setelement('totalprice2',total);
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+document.getElementById('dcard1').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('accessories');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    if(v1>0){
+       var total=prevtotalFloat - prevvalueFloat;    
+    var newtotal=total;
+   v1--; 
+    }
+    else{
+        var total=prevtotalFloat;    
+        var newtotal=total; 
+    }
+    
+    setelement('it1',v1);
+    setelement('totalprice',total);   
+    setelement('totalprice2',total);   
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+
+
+  
+document.getElementById('card2').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('products');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;   
+    v2++;
+    setelement('it2',v2);
+    setelement('totalprice',total);setelement('totalprice2',total); 
+    setelement('total3',newtotal);
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+})
+document.getElementById('car2').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('products');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;   
+    v2++;
+    setelement('it2',v2);
+    setelement('totalprice',total); setelement('totalprice2',total);
+    setelement('total3',newtotal);
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+})
+document.getElementById('dcard2').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('products');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    if(v2>0){
+       var total=prevtotalFloat - prevvalueFloat;    
+    var newtotal=total;
+   v2--; 
+    }
+    else{
+        var total=prevtotalFloat;    
+        var newtotal=total; 
+    }
+    
+    setelement('it2',v2);
+    setelement('totalprice',total);  setelement('totalprice2',total);  
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+document.getElementById('card3').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('cooker');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v3++;
+    
+    setelement('it3',v3);
+    setelement('totalprice',total);setelement('totalprice2',total); 
+    setelement('total2',newtotal);
+    setelement('total',newtotal);
+    setelement('total3',newtotal);
+})
+document.getElementById('car3').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('cooker');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v3++;
+    
+    setelement('it3',v3);
+    setelement('totalprice',total); setelement('totalprice2',total);
+    setelement('total2',newtotal);
+    setelement('total',newtotal);
+    setelement('total3',newtotal);
+})
+document.getElementById('dcard3').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('cooker');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    if(v3>0){
+       var total=prevtotalFloat - prevvalueFloat;    
+    var newtotal=total;
+   v3--; 
+    }
+    else{
+        var total=prevtotalFloat;    
+        var newtotal=total; 
+    }
+    
+    setelement('it3',v3);
+    setelement('totalprice',total); setelement('totalprice2',total);   
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+
+document.getElementById('card4').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('cap');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v4++;
+    setelement('it4',v4);
+    setelement('totalprice',total);setelement('totalprice2',total); 
+    setelement('total2',newtotal);
+    setelement('total',newtotal);
+    setelement('total3',newtotal);
+})
+document.getElementById('car4').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('cap');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v4++;
+    setelement('it4',v4);
+    setelement('totalprice',total); setelement('totalprice2',total);
+    setelement('total2',newtotal);
+    setelement('total',newtotal);
+    setelement('total3',newtotal);
+})
+document.getElementById('dcard4').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('cap');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    if(v4>0){
+       var total=prevtotalFloat - prevvalueFloat;    
+    var newtotal=total;
+   v4--; 
+    }
+    else{
+        var total=prevtotalFloat;    
+        var newtotal=total; 
+    }
+    
+    setelement('it4',v4);
+    setelement('totalprice',total); setelement('totalprice2',total);   
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+document.getElementById('card5').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('jersy');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v5++;
+    setelement('it5',v5);
+    setelement('totalprice',total);setelement('totalprice2',total); 
+    setelement('total2',newtotal);
+    setelement('total',newtotal);
+    setelement('total3',newtotal);
+})
+document.getElementById('car5').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('jersy');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v5++;
+    setelement('it5',v5);
+    setelement('totalprice',total); setelement('totalprice2',total);
+    setelement('total2',newtotal);
+    setelement('total',newtotal);
+    setelement('total3',newtotal);
+})
+document.getElementById('dcard5').addEventListener('click',function(){
+    
+    const prevvalue=getelementbytext('jersy');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    if(v5>0){
+       var total=prevtotalFloat - prevvalueFloat;    
+    var newtotal=total;
+   v5--; 
+    }
+    else{
+        var total=prevtotalFloat;    
+        var newtotal=total; 
+    }
+    
+    setelement('it5',v5);
+    setelement('totalprice',total); setelement('totalprice2',total);   
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+document.getElementById('card6').addEventListener('click',function(){    
+    const prevvalue=getelementbytext('cates');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v6++;
+    setelement('it6',v6);
+    setelement('totalprice',total);setelement('totalprice2',total); 
+    setelement('total3',newtotal);
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+})
+document.getElementById('car6').addEventListener('click',function(){    
+    const prevvalue=getelementbytext('cates');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    const total=prevvalueFloat+prevtotalFloat;  
+    const newtotal=total;
+    v6++;
+    setelement('it6',v6);
+    setelement('totalprice',total); setelement('totalprice2',total);
+    setelement('total3',newtotal);
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+})
+document.getElementById('dcard6').addEventListener('click',function(){   
+    const prevvalue=getelementbytext('cates');
+    const prevvalueFloat=parseFloat(prevvalue);
+    const prevtotal=getelementbytext('totalprice');
+    const prevtotalFloat=parseFloat(prevtotal);
+    if(v6>0){
+       var total=prevtotalFloat - prevvalueFloat;    
+    var newtotal=total;
+   v6--; 
+    }
+    else{
+        var total=prevtotalFloat;    
+        var newtotal=total; 
+    }
+    
+    setelement('it6',v6);
+    setelement('totalprice',total); setelement('totalprice2',total);   
+    setelement('total',newtotal);
+    setelement('total2',newtotal);
+    setelement('total3',newtotal); 
+})
+document.getElementById('btn-purchase').addEventListener('click',function(){
+    
+    const prevtotal=getelementbytext('totalprice');
+    const total=parseFloat(prevtotal); 
+    const mm=0; 
+     
+   if(total>0.0){ 
+   
+    show();   
+   }
+   })
+document.getElementById('home').addEventListener('click',function(){ 
+    hide();  
+ 
+   })
+document.getElementById('confirm').addEventListener('click',function(){   
+    hide2();  
+   })
+document.getElementById('n-confirm').addEventListener('click',function(){
+    hide3();  
+   })
+document.getElementById('bkash-img').addEventListener('click',function(){
+    hideb();  
+   })
+document.getElementById('nagad-img').addEventListener('click',function(){
+    hiden();  
+   })
+
+
+   
